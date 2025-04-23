94e2728887acbbcc97d0cf7ecc028d7a9fe447c9
unknown <andresmorenochechomoreno1995@gmail.com>
2025-04-23 18:17:17 -0500

    mi comiindsas

--- rdcm-matricula/src/main/java/org/example/rdcmmatricula/MsMatriculaApplication.java
+++ rdcm-matricula/src/main/java/org/example/rdcmmatricula/MsMatriculaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
+@EnableFeignClients(basePackages = "org.example.rdcmmatricula.feign")
 @SpringBootApplication
 public class MsMatriculaApplication {
     public static void main(String[] args) {
