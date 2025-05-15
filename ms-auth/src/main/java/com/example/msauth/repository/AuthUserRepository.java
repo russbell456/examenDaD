bce7ee38e628ee33731b9f03d7cbe89b71b8315c
unknown <andresmorenochechomoreno1995@gmail.com>
2025-05-15 08:35:23 -0500

    dasadas

--- ms-auth/src/main/java/com/example/msauth/repository/AuthUserRepository.java
+++ ms-auth/src/main/java/com/example/msauth/repository/AuthUserRepository.java
@@ -4,7 +4,10 @@ package com.example.msauth.repository;
 import com.example.msauth.entity.AuthUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 
-public interface AuthUserRepository extends JpaRepository<AuthUser,Integer> {
-    AuthUser findByUsername(String username);
+
+public interface AuthUserRepository extends JpaRepository<AuthUser, Integer> {
+    Optional<AuthUser> findByUserName(String userName);
 }
+
